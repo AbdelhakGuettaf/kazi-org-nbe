e4e2990bc7cc9722ea8a18534a6a96d633a6410e
Shocky <hakotaf@gmail.com>
2023-10-02 11:45:22 +0100

    updated prisma and added cert bundle

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,10 @@
+import { PrismaClient } from '@prisma/client';
 import { Router } from 'express';
 
-const api = Router();
+const api = Router().use('/test', async (req, res) => {
+  const prisma = new PrismaClient();
+  const t = await prisma.user.create({ data: { userId: 'test' } });
+
+  res.status(200).json(t);
+});
 export default Router().use('/api', api);
